1f63cae92b2a0429ca2be06453a9996ab22ed17e
Umit Turk <uyt.turk@gmail.com>
2022-11-20 17:08:42 +0300

    created the photo model

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import conn from './db.js'
-
+import conn from './db.js';
+import pageRoute from './routes/pageRoute.js';
+import photoRoute from './routes/photoRoute.js';
 //We can access dotenv file
 dotenv.config();
 
@@ -17,12 +18,10 @@ app.set('view engine', "ejs");
 //static files middleware
 app.use(express.static('public'))
 
-app.get('/',(req, res) => {
-    res.render('index');
-})
-app.get('/about',(req, res) => {
-    res.render('about')
-})
+//routes
+app.use("/",pageRoute);
+app.use("/photos",photoRoute);
+
 
 app.listen(port, () => {
     console.log(`Application running on port: ${port}`);
